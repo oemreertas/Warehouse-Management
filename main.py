f51724698e3ac4f978bdf37aa5b3b827d13789bf
Özgür Emre Ertaş <oemreertas@gmail.com>
2024-04-12 15:00:13 +0300

    Dashboard Revised-3

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
 
-class DashBoard:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Warehouse Management")
-        self.root.config(bg="#fffafa")
-        window_width = 1366
-        window_height = 768
-        self.root.geometry(f"{window_width}x{window_height}+"
-                           f"{root.winfo_screenwidth() // 2 -
-                              window_width // 2}+{root.winfo_screenheight() // 2 - window_height // 2}")
-        self.root.resizable(False, False)
-        '''====================SIDE BAR=============================='''
-        'Frame Options'
-
-        self.SideFrame = ttk.Frame(root, style="Custom.TFrame", width=268, height=768)
-        self.SideFrame.pack(side="left", fill="y", expand=False)
-
-        self.style = ttk.Style()
-        self.style.theme_use('alt')
-
-        "Buttons Style"
-        self.style.configure("Custom.TButton", foreground="white", font=("Helvetica", 12, "bold"),
-                             background='#2f4f4f', borderwidth=0)
-        self.style.map('Custom.TButton',
-                       background=[('active', '#2f4f4f'), ('disabled', '#f0f0f0')])
-
-        "Side Bar Frame Style"
-        self.style.configure("Custom.TFrame", background="#2f4f4f")
-
-        self.button1 = ttk.Button(self.SideFrame, text="BUTTON1", style="Custom.TButton", takefocus=False,
-                                  width=28, padding=4)
-        self.button1.place(x=0, y=170)
+def create_button(self, text, command, x, y):
+    button = tk.Button(self, text=text, command=command)
+    button.configure(bg="#1c6071", fg="white", font=("Arial", 15, "bold"),
+                     takefocus=False, bd=0, cursor="hand2", activebackground="#1c6071",
+                     activeforeground="gray")
+    button.place(x=x, y=y)
+    return button
 
 
-def wind():
-    root = Tk()
-    DashBoard(root)
-    root.mainloop()
+class Dashboard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Warehouse Management")
+        self.geometry("1366x768+{}+{}".format(self.winfo_screenwidth() // 2 - 1366 // 2,
+                                              self.winfo_screenheight() // 2 - 768 // 2))
+        self.resizable(False, False)
+        self.config(bg="#fffafa")
+
+        self.create_sidebar()
+
+    def create_sidebar(self):
+        sidebar_frame = tk.Frame(self, bg="#1c6071", width=240, height=768)
+        sidebar_frame.pack(side="left")
+
+        create_button(sidebar_frame, "Dashboard", self.do_nothing, x=50, y=50)
+        create_button(sidebar_frame, "Orders", self.do_nothing, x=50, y=100)
+
+    def do_nothing(self):
+        pass
 
 
 if __name__ == '__main__':
-    wind()
+    app = Dashboard()
+    app.mainloop()
